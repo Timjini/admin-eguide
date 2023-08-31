b171ebcaa3bd35f166156f2ec4560cf05ccf9101
Timjini <hatim.jini@gmail.com>
2023-09-01 00:24:37 +0300

    missing /

--- src/constant/index.js
+++ src/constant/index.js
@@ -4,7 +4,7 @@ export const HEADERS = {
     Authorization: `Bearer ${token}`
   };
   // const baseUrl = 'http://localhost:4000/'
-  const baseUrl = 'https://stream-ten-eta.vercel.app';
+  const baseUrl = 'https://stream-ten-eta.vercel.app/';
   
   
 export const API_ROOT = `${baseUrl}api`; 

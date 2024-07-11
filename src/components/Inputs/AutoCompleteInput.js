da212ef8794d8c40ad745f0f9eda6aee2a2c011d
Smartup Network <smartupnetwork@Hatim-Laajini.fritz.box>
2024-07-11 22:16:55 +0200

    auto complete input:

--- src/components/Inputs/AutoCompleteInput.js
+++ src/components/Inputs/AutoCompleteInput.js
@@ -1,17 +1,85 @@
 import React, { useState, useEffect, useRef } from "react";
-import Autocomplete from "react-google-autocomplete";
+import { createRoot } from "react-dom/client";
+import {
+  APIProvider,
+  ControlPosition,
+  MapControl,
+  AdvancedMarker,
+  Map,
+  useMap,
+  useMapsLibrary,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
 const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const AutoCompleteInput = () => {
-
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [markerRef, marker] = useAdvancedMarkerRef();
   return (
-    <Autocomplete
-    apiKey={API_KEY}
-    onPlaceSelected={(place) => {
-      console.log(place);
-    }}
-  />
-  )
-}
+    <APIProvider
+      apiKey={API_KEY}
+      solutionChannel="GMP_devsite_samples_v3_rgmautocomplete"
+    >
+      <Map
+        mapId={"bf51a910020fa25a"}
+        defaultZoom={3}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+      >
+        <AdvancedMarker ref={markerRef} position={null} />
+      </Map>
+      <MapControl position={ControlPosition.TOP}>
+        <div className="autocomplete-control">
+          <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+        </div>
+      </MapControl>
+      <MapHandler place={selectedPlace} marker={marker} />
+    </APIProvider>
+  );
+};
+
+const MapHandler = ({ place, marker }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map || !place || !marker) return;
+
+    if (place.geometry?.viewport) {
+      map.fitBounds(place.geometry?.viewport);
+    }
+
+    marker.position = place.geometry?.location;
+  }, [map, place, marker]);
+  return null;
+};
+
+const PlaceAutocomplete = ({ onPlaceSelect }) => {
+  const [placeAutocomplete, setPlaceAutocomplete] = useState(null);
+  const inputRef = useRef(null);
+  const places = useMapsLibrary("places");
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+
+    const options = {
+      fields: ["geometry", "name", "formatted_address"],
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+  }, [places]);
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+
+    placeAutocomplete.addListener("place_changed", () => {
+      onPlaceSelect(placeAutocomplete.getPlace());
+    });
+  }, [onPlaceSelect, placeAutocomplete]);
+  return (
+    <div className="autocomplete-container">
+      <input ref={inputRef} />
+    </div>
+  );
+};
 
 export default AutoCompleteInput;

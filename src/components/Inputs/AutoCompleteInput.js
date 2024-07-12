c74ddbf8f4a41f3975413d9ae60f65282ddd738d
Necmi Gunduz <necmigunduz@gmail.com>
2024-07-12 16:27:43 +0300

    add: basic google map autocomplete

--- src/components/Inputs/AutoCompleteInput.js
+++ src/components/Inputs/AutoCompleteInput.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { LoadScript, StandaloneSearchBox } from "@react-google-maps/api";
 
 const LIB = ['places'];
@@ -6,39 +6,29 @@ const LIB = ['places'];
 const AutoCompleteInput = () => {
   const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   const [searchBox, setSearchBox] = useState(null);
-  const [address, setAddress] = useState('');
-  const inputRef = useRef(null);
 
   const onPlacesChanged = () => {
     const places = searchBox?.getPlaces();
-    console.log('Places:', places);
     if (places && places.length > 0) {
-      setAddress(places[0].formatted_address);
+      console.log(places[0].formatted_address);
     }
   };
 
   const onSBLoad = (ref) => {
-    console.log('SearchBox loaded:', ref);
     setSearchBox(ref);
   };
 
   return (
-    <LoadScript
-      googleMapsApiKey={API_KEY}
-      libraries={LIB}
-    >
-      <StandaloneSearchBox
-        onLoad={onSBLoad}
-        onPlacesChanged={onPlacesChanged}
-      >
+    <LoadScript googleMapsApiKey={API_KEY} libraries={LIB}>
+      <StandaloneSearchBox onLoad={onSBLoad} onPlacesChanged={onPlacesChanged}>
         <input
           type="text"
           placeholder="Business name, address, town or zip code"
-          ref={inputRef}
+          style={{ width: '100%', padding: '10px', fontSize: '16px' }}
         />
       </StandaloneSearchBox>
     </LoadScript>
   );
-}
+};
 
 export default AutoCompleteInput;

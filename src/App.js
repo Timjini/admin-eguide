13523df3bea11c800c001173a726502648883e5b
Timjini <hatim.jini@gmail.com>
2024-09-15 10:46:57 +0200

    tours crud

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ import CreateAgency from './agency/management/CreateAgency';
 import AllChannels from './admin/views/AllChannel';
 import ErrorPage from './pages/ErrorPage';
 import AgencyPage from './agency/views/AgencyPage';
+import TourPage from './agency/views/TourPage';
+
 
 function App() {
   const auth = useSelector(state => state.user);  
@@ -39,6 +41,7 @@ function App() {
               <Route path="/agency/channel/create" element={<ChannelCreate/>} />
               <Route path="/admin/agencies" element={<AdminAgencies/>} />
               <Route path="/admin/agencies/:agencyId" element={<AgencyPage/>} />
+              <Route path="/admin/tour/:tourId" element={<TourPage/>} />
               <Route path="/admin/new" element={<NewTable />} />
             </>
           ) : (

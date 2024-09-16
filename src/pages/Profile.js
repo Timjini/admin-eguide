208a618d6b2dbb519ccf3c3a0ccce156897812f9
Necmi Gunduz <necmigunduz@gmail.com>
2024-09-16 17:20:36 +0300

    reshape: button in User Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -199,7 +199,7 @@ export default function Profile() {
                                 </div>
                                 <div className="col-span-6 sm:col-full">
                                     <button
-                                        className=" font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
+                                        class="bg-white hover:bg-gray-600 text-gray-800 hover:text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow"
                                         type="submit"
                                     >
                                         Save all

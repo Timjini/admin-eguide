9013760386afeb4b94886bfe0e16da0a10375ac0
Timjini <hatim.jini@gmail.com>
2024-09-23 00:19:18 +0200

    add subscriptions

--- src/admin/views/PaymentsPage.js
+++ src/admin/views/PaymentsPage.js
@@ -1,8 +1,98 @@
-const PaymentsPage = () => {
+import React, { useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination } from '@mui/material';
+import useGetAdminPayments from '../../hooks/useGetAdminPayments';
+import Loader from '../../components/Loaders/Loader';
+import BackButton from '../../components/Buttons/BackButton';
 
-    return (
-        <></>
-    );
-}
+const PaymentsPage = () => {
+  const { payments, loading, error } = useGetAdminPayments();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (payments.length === 0) {
+    return <p>No payments found</p>;
+  }
+
+  const formattedDate = (dateString) => {
+    const options = { day: 'numeric', month: 'numeric', year: '2-digit' };
+    return new Intl.DateTimeFormat('en-GB', options).format(new Date(dateString));
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
+  const paginatedPayments = payments.data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
+  return (
+    <div className="p-4 flex flex-col content-wrapper">
+      <div className='flex flex-row justify-between'>
+        <BackButton />
+      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="channels table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Payment Status</TableCell>
+              <TableCell>Payment Date</TableCell>
+              <TableCell>Amount Paid</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paginatedPayments.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={5} className="p-4">
+                  No data available
+                </TableCell>
+              </TableRow>
+            ) : (
+                paginatedPayments.map((payment, index) => (
+                <TableRow key={index}>
+                  <TableCell>{payment.status}</TableCell>
+                  <TableCell>{payment.paymentDate ? formattedDate(payment.paymentDate) : '-'}</TableCell>
+                  <TableCell>{payment.amount }</TableCell>
+                  <TableCell className="px-6 py-4 flex gap-2">
+                    <button id={payment._id}>
+                      <span className="material-symbols-outlined">visibility</span>
+                    </button>
+                    <button id={payment._id}>
+                      <span className="material-symbols-outlined">edit</span>
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+
+        {/* Table Pagination */}
+        <TablePagination
+          component="div"
+          count={payments.data.length}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          rowsPerPageOptions={[5, 10, 25]}
+        />
+      </TableContainer>
+    </div>
+  );
+};
 
 export default PaymentsPage;
--- src/api/admin.js
+++ src/api/admin.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {API_ROOT} from '../constant'
+import {API_ROOT, API_VERSION_2} from '../constant'
 
 
 const getAdminDashboardData = payload => axios.get(`${API_ROOT}/admin/dashboard`, payload);
@@ -7,6 +7,9 @@ const getAdminChannelsData = payload => axios.get(`${API_ROOT}/admin/channels`, 
 const getAdminToursData = payload => axios.get(`${API_ROOT}/admin/tours`, payload);
 const getAdminGuidesData = payload => axios.get(`${API_ROOT}/admin/guides`, payload);
 const createAgencyOwner = payload => axios.post(`${API_ROOT}/admin/create_agency_owner`, payload);
+const getPayments = payload => axios.get(`${API_VERSION_2}/payments`, payload);
+const getSubscriptions = payload => axios.get(`${API_VERSION_2}/subscriptions`, payload);
+
 
 
 const adminApi = {
@@ -14,7 +17,9 @@ const adminApi = {
     getAdminChannelsData,
     getAdminToursData,
     getAdminGuidesData,
-    createAgencyOwner
+    createAgencyOwner,
+    getPayments,
+    getSubscriptions
   };
   
 export default adminApi;
